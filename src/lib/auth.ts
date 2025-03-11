24f47b624ffa21b4c9e4682167aa66818bf89f5c
Ykolo <loiclau2005@gmail.com>
2025-03-11 11:33:14 +0100

    feat: auth.js

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,9 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import { prisma } from './prisma';
- 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  session: {strategy: 'jwt'}, 
   providers: [],
 });
